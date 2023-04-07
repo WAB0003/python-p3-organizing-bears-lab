1a0ba720dac2b319277b612c166c3b93e323e082
Bill Brown <bill.a.brown90@gmail.com>
2023-04-07 16:25:57 -0700

    Practicing SQL

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 15, "M", "brown", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "M", "black", "mean", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 15, "F", "brown", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 15, "M", "brown", "nice", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 15, "F", "brown", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 15, "M", "brown", "mean", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 15, "F", "brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
